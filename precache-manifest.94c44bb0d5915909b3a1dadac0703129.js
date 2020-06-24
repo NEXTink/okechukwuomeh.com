b096b0ee0574531dd07dd80208cdfa4d22436abd
NEXTink <okechukwuomeh3@gmail.com>
2020-06-24 17:50:10 +0100

    Updates

---
+++ precache-manifest.94c44bb0d5915909b3a1dadac0703129.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e52b2eea033c095847fc26737319d29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a75ae7994ca64a2fa501",
+    "url": "/static/css/main.889518ac.chunk.css"
+  },
+  {
+    "revision": "ab7dbd039f6e49c64dd4",
+    "url": "/static/js/2.392baf75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.392baf75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a75ae7994ca64a2fa501",
+    "url": "/static/js/main.cfe976d1.chunk.js"
+  },
+  {
+    "revision": "bbbdf4e6eba113698396",
+    "url": "/static/js/runtime-main.18ad118e.js"
+  },
+  {
+    "revision": "15d6661dadbb69d9ec61ec959fada575",
+    "url": "/static/media/Register.15d6661d.png"
+  },
+  {
+    "revision": "54526a1d5b089a75d04a241ccf70c969",
+    "url": "/static/media/astrialawTest_home.54526a1d.png"
+  },
+  {
+    "revision": "b29f578eb88b808563590577c4e65d67",
+    "url": "/static/media/login.b29f578e.png"
+  },
+  {
+    "revision": "0dd11745c408e471d450e7999f4f7e31",
+    "url": "/static/media/me.0dd11745.jpg"
+  }
+]);
